e7b982c9f4d85d66a58bb4e31d130fd7032bf899
Alexandre Lipari <santuslipari@gmail.com>
2022-08-26 13:25:15 -0300

    Guardar resultados no array

--- lista16.js
+++ lista16.js
@@ -2,6 +2,7 @@ var A = 0
 var B = 0
 var contador = 0
 var desejaContinuar = 1
+var array = []
 while (desejaContinuar == 1){
 A = parseInt(prompt("insira o primeiro número"))
 B = parseInt(prompt("insira o segundo número"))
@@ -9,19 +10,24 @@ B = parseInt(prompt("insira o segundo número"))
 var opcao = prompt("qual operação deseja? Soma (1) Subtração (2) Multiplicação (3) Divisão (4)")
 
 if (opcao == 1){
-console.log("Soma dos valores:") , Somar (A , B)
+array[contador] = Somar (A , B)   
+console.log("Soma dos valores:") , array[contador]
 contador++
 }else if (opcao == 2){
-console.log("Subtração dos valores:") , Subtrair (A , B)
+array[contador] = Subtrair (A , B)  
+console.log("Subtração dos valores:") , array[contador]
 contador++    
 }else if (opcao == 3){
-console.log("Multiplicação dos valores:") , Multiplicar (A , B)
+array[contador] = Multiplicar (A , B)
+console.log("Multiplicação dos valores:") , array[contador]
 contador++    
 }else if (opcao == 4){
-console.log("Divisão dos valores:") , Dividir (A , B)
+array[contador] = Dividir (A , B)
+console.log("Divisão dos valores:") , array[contador]
 contador++    
 }
 desejaContinuar = prompt("deseja continuar? Digite: (1) SIM (2) NÃO")
+
 }
 function Somar ( A , B ){
     return A + B
@@ -35,4 +41,4 @@ function Multiplicar ( A , B ){
 function Dividir ( A , B ){
     return A / B
 }
-console.log(Somar (A , B))
+console.log(array)
