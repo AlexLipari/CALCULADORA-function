aa898348a0d2172e8bb67cff22d3ea42f1d2f961
Alexandre Lipari <santuslipari@gmail.com>
2022-08-26 00:09:52 -0300

    Algoritimo para inserir numeros e function dos calculos e return

---
+++ lista16.js
@@ -0,0 +1,22 @@
+var A = 0
+var B = 0
+
+
+A = parseInt(prompt("insira o primeiro número"))
+B = parseInt(prompt("insira o segundo número"))
+
+
+
+
+function Somar ( num1 , num2 ){
+    return num1 + num2
+}
+function subtrair ( num1 , num2 ){
+    return num1 - num2
+}
+function multiplicar ( num1 , num2 ){
+    return num1 * num2
+}
+function dividir ( num1 , num2 ){
+    
+}
