36f4101aa5e35d74f2a83d250a67c5313f2caf64
Alexandre Lipari <santuslipari@gmail.com>
2022-08-26 14:20:02 -0300

    Exibir historico do resultado

--- lista16.js
+++ lista16.js
@@ -3,42 +3,52 @@ var B = 0
 var contador = 0
 var desejaContinuar = 1
 var array = []
-while (desejaContinuar == 1){
-A = parseInt(prompt("insira o primeiro número"))
-B = parseInt(prompt("insira o segundo número"))
+while (desejaContinuar == 1) {
+    A = parseInt(prompt("insira o primeiro número"))
+    B = parseInt(prompt("insira o segundo número"))
 
-var opcao = prompt("qual operação deseja? Soma (1) Subtração (2) Multiplicação (3) Divisão (4)")
+    var opcao = prompt("qual operação deseja? Soma (1) Subtração (2) Multiplicação (3) Divisão (4) Exibir (5)")
 
-if (opcao == 1){
-array[contador] = Somar (A , B)   
-console.log("Soma dos valores:") , array[contador]
-contador++
-}else if (opcao == 2){
-array[contador] = Subtrair (A , B)  
-console.log("Subtração dos valores:") , array[contador]
-contador++    
-}else if (opcao == 3){
-array[contador] = Multiplicar (A , B)
-console.log("Multiplicação dos valores:") , array[contador]
-contador++    
-}else if (opcao == 4){
-array[contador] = Dividir (A , B)
-console.log("Divisão dos valores:") , array[contador]
-contador++    
+    if (opcao == 1) {
+        array[contador] = Somar(A, B)
+        console.log("Soma dos valores:"), array[contador]
+        contador++
+    } else if (opcao == 2) {
+        array[contador] = Subtrair(A, B)
+        console.log("Subtração dos valores:"), array[contador]
+        contador++
+    } else if (opcao == 3) {
+        array[contador] = Multiplicar(A, B)
+        console.log("Multiplicação dos valores:"), array[contador]
+        contador++
+    } else if (opcao == 4) {
+        array[contador] = Dividir(A, B)
+        console.log("Divisão dos valores:"), array[contador]
+        contador++
+    } else if (opcao == 5) {
+        histResult()
+    } else {
+        desejaContinuar = 2
+    }
+    desejaContinuar = prompt("deseja continuar? Digite: (1) SIM (2) NÃO")
+
 }
-desejaContinuar = prompt("deseja continuar? Digite: (1) SIM (2) NÃO")
-
+function histResult() {
+    for (var i = 0; i < contador; i++) {
+        console.log(array[i])
+    }
 }
-function Somar ( A , B ){
+function Somar(A, B) {
     return A + B
 }
-function Subtrair ( A , B ){
+function Subtrair(A, B) {
     return A - B
 }
-function Multiplicar ( A , B ){
+function Multiplicar(A, B) {
     return A * B
 }
-function Dividir ( A , B ){
+function Dividir(A, B) {
     return A / B
 }
 console.log(array)
+console.log(histResult())
